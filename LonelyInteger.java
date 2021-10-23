12524094a9de714fc5f6d4e1f4394e99ef2110df
IamSanketMore <sanket@ideaclan.biz>
2021-10-23 18:16:08 +0530

    [HackerRank-Solution]- Lonenly Integer Problem

---
+++ LonelyInteger.java
@@ -0,0 +1,90 @@
+/*
+Given an array of integers, where all elements but one occur twice, find the unique element.
+
+Example
+
+The unique element is .
+
+Function Description
+
+Complete the lonelyinteger function in the editor below.
+
+lonelyinteger has the following parameter(s):
+
+int a[n]: an array of integers
+Returns
+
+int: the element that occurs only once
+Input Format
+
+The first line contains a single integer, , the number of integers in the array.
+The second line contains  space-separated integers that describe the values in .
+
+example
+int a[] ={1,2,2,1,4,3,3}
+ouput= 4
+*/
+------------------------------------------ Program-------------------------------------------------------------------------------
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class LonelyInteger {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+    // Write your code here
+
+        HashMap<Integer, Integer> m = new HashMap<>();
+        long sum1 = 0, sum2 = 0;
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (!m.containsKey(a.get(i)))
+            {
+                sum1 += a.get(i);
+                m.put(a.get(i), 1);
+            }
+            sum2 += a.get(i);
+        }
+ 
+        // applying the formula.
+        return (int)(2 * (sum1) - sum2);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
